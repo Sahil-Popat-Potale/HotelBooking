713acea22d5d9f34aa1f5fe210e77d9ca4a7763e
SAHIL-PC <sahilpopat97@gmail.com>
2023-05-05 10:15:20 +0530

    Initial commit

---
+++ app/src/main/java/sahilpopat/nausheen/hotelbooking/LoginActivity.java
@@ -0,0 +1,23 @@
+package sahilpopat.nausheen.hotelbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //Line no.16 is suppose to get the string from Main activity java
+        String infor=getIntent().getStringExtra("info");
+        TextView txv=findViewById(R.id.textView);
+        txv.setText("Room Type: "+infor);
+
+        btn.findViewById(R.id.submit);
+        btn.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, HomeActivity.class)));
+    }
+}
---
+++ app/src/main/java/sahilpopat/nausheen/hotelbooking/MainActivity.java
@@ -0,0 +1,22 @@
+package sahilpopat.nausheen.hotelbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnbook;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnbook=findViewById(R.id.button);
+        btnbook.setOnClickListener(v -> {
+            Intent i= new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
+            i.putExtra("info", "Single Bed Rooms");
+        });
+    }
+}
