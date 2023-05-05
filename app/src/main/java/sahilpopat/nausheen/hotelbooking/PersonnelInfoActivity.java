6478d11a923a8a469a5f43d296553e95570dda7f
SAHIL-PC <sahilpopat97@gmail.com>
2023-05-05 15:32:26 +0530

    added remaining code

--- app/src/main/java/sahilpopat/nausheen/hotelbooking/PersonnelInfoActivity.java
+++ app/src/main/java/sahilpopat/nausheen/hotelbooking/PersonnelInfoActivity.java
@@ -6,18 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class PersonnelInfoActivity extends AppCompatActivity {
-
+    EditText name, email, phone, people, address;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personnel_info);
-        Button save = findViewById(R.id.submit);
+        Button save = findViewById(R.id.Submit);
+        name=findViewById(R.id.Name);
+        email=findViewById(R.id.Email);
+        phone=findViewById(R.id.Phone);
+        people=findViewById(R.id.People);
+        address=findViewById(R.id.Address);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(PersonnelInfoActivity.this, RoomInfoActivity.class);
+                i.putExtra("name",name.getText().toString());
+                i.putExtra("email",email.getText().toString());
+                i.putExtra("phone",phone.getText().toString());
+                i.putExtra("people",people.getText().toString());
+                i.putExtra("address",address.getText().toString());
                 startActivity(i);
             }
         });
--- app/src/main/java/sahilpopat/nausheen/hotelbooking/RoomInfoActivity.java
+++ app/src/main/java/sahilpopat/nausheen/hotelbooking/RoomInfoActivity.java
@@ -2,13 +2,64 @@ package sahilpopat.nausheen.hotelbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class RoomInfoActivity extends AppCompatActivity {
-
+    Calendar myCalendar;
+    EditText checkIn, checkOut;
+    String name, email, phone, people, address;
+    Spinner spinner;
+    Button preview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_info);
+
+        Intent i=getIntent();
+        name=i.getStringExtra("name");
+        email=i.getStringExtra("email");
+        phone=i.getStringExtra("phone");
+        people=i.getStringExtra("people");
+        address=i.getStringExtra("address");
+
+        myCalendar = Calendar.getInstance();
+        checkIn=findViewById(R.id.CheckIn);
+        checkOut=findViewById(R.id.CheckOut);
+        spinner=findViewById(R.id.spinnerType);
+        preview=findViewById(R.id.Preview);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLable();
+            }
+        };
+        checkIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(RoomInfoActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+    private void updateLable(){
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf= new SimpleDateFormat(myFormat, Locale.US);
+
+        checkIn.setText(sdf.format(myCalendar.getTime()));
     }
 }
